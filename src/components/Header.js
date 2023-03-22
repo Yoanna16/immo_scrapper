f45d538cec25f64db5d6dcdec1f9f01722a2bc07
Yoanna16 <yoannaborisova@gmail.com>
2023-03-22 10:38:02 +0100

    Add general styling and header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <div className='header'>
+            <div>
+            <h3 className='header-title'>Room Search</h3>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
